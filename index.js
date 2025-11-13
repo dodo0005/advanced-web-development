781ba39b770834942ffb52a04ac465afce10e725
Mahmoud NASSER <mahmoud1.nasser@epita.fr>
2025-11-13 23:31:26 +0100

    new middleware

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
+import { logMiddleware } from "./src/middleware/middleware.js"
 import express from "express"
 const app = express()
 const PORT = 3000
-app.use(express.json())
-app.get("/", (req, res) => {
-    res.json({
-	    firstName: "John",
-	    lastName: "Doe"
-    })
+
+const users = [
+	{ id: 1, name: "Alice" },
+	{ id: 2, name: "Bob" },
+	{ id: 3, name: "Charlie" },
+	{ id: 4, name: "Dave" },
+]
+
+
+// middleware to log request body
+// middleware to log request body
+// app.use(async (req, res, next) => {
+// 	const date = new Date().toISOString()
+// 	console.log(`[${date}] ${req.method} ${req.url}`)
+
+	
+// 	const response = await fetch("https://jsonplaceholder.typicode.com/users/1")
+// 	const data = await response.json()
+// 	req.data = data
+// 	console.log(data)
+	
+// 	next()
+// })
+
+app.get("/", logMiddleware, (req, res) => {
+	res.json({ users, data:req.data })
 })
+
+app.post("/", (req, res) => {
+	console.log(req)
+})
+
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`)
+	console.log(`Server is running on http://localhost:${PORT}`)
 })
